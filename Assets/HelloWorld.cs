252fd976e616cbf2448c807e8b15c8518deeba38
rikkrolled <46076727+rikkrolled@users.noreply.github.com>
2018-12-23 09:08:29 +0000

    Initial commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello ERic. This is before I click on Mac. \n Testing this one more time.\nI'm going to try and add this line.");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
